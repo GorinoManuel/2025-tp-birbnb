df8433d8a4926bec9cbae75da627699bf406764e
Gaston Gonzalez Sapia <ggonzalezsapia@gmail.com>
2025-07-07 21:11:34 -0300

    se completan los test E2E

--- client/cypress/e2e/busqueda_alojamientos.cy.js
+++ client/cypress/e2e/busqueda_alojamientos.cy.js
@@ -2,7 +2,6 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
 
-    //cy.get('.carousel > div:nth-child(2) .item-footer-actions div button:nth-of-type(2)').click()
     cy.get('.appbar-buttons > button:nth-child(1)').click()
 
     cy.get('.barra-busqueda > button:nth-child(5)').click()
@@ -11,11 +10,39 @@ describe('template spec', () => {
 
     cy.get('div:nth-child(5)').click()
 
-    cy.get('.barra-busqueda > button:nth-child(8)').click()
+    const botonBuscar = cy.get('.barra-busqueda > button:nth-child(8)')
+    botonBuscar.should('contain', 'Buscar')
+    botonBuscar.click()
 
     const primerCard = cy.get('.carousel .cardlist > div:nth-child(1)')
     primerCard.should('contain', 'Brasil')
 
-    // se debe agregar resto del recorrido hasta reservar
+
+    const botonVerMas = cy.get('.cardAlojamiento > div:nth-child(3) .cardButton div button')
+    botonVerMas.should('contain', 'ver mas +')
+    botonVerMas.click()
+
+    // Alojamientos Detail
+    const imagenAlojamiento = cy.get('.image')
+    imagenAlojamiento.find('img').should('be.visible')
+
+
+    // Descripcion del alojamiento
+    cy.get('.image-footer .description').find('.chips-box').should('be.visible')
+    cy.get('.image-footer .description').find('h1').should('be.visible')
+
+    cy.get('.image-footer .description > p:nth-child(4)').should('include.text','Dirección:')
+    cy.get('.image-footer .description > p:nth-child(5)').should('include.text','Precio por noche:')
+    cy.get('.image-footer .description > p:nth-child(6)').should('include.text','Horario de Check In:')
+    cy.get('.image-footer .description > p:nth-child(7)').should('include.text','Horario de Check Out:')
+    cy.get('.image-footer .description > p:nth-child(8)').should('include.text','huesped/es')
+    
+    // Boton de reservar
+    cy.get('.image-footer .buying-section form > div:nth-child(1)').type(1)
+    cy.get('.image-footer .buying-section form > div:nth-child(2)').type('2025-09-17')
+    cy.get('.image-footer .buying-section form > div:nth-child(3)').type('2025-09-21')
+
+    cy.get('.image-footer .buying-section form > button').should('contain', 'Reservar')
+    cy.get('.image-footer .buying-section form > button').click()
   })
 })
